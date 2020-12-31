7ee46fdd269ff9a77f5f6977daa24fbb919f632a
Trung Vu <trung@apitracker.com>
2020-12-31 11:41:47 -0600

    use latest middleware, add handler to test track & identify

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import uuid
 
+import datetime
 import logging
 import os
 
@@ -15,13 +17,30 @@ import hoss_agent.middleware.tornado as HossMiddleware
 
 define("port", default="8000", help="Listening port", type=str)
 
+hoss_middleware = None
 
 class MainHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
         http_client = AsyncHTTPClient()
         response = yield http_client.fetch("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
-        self.write(response.body)
+        self.write(dict(foo="bar"))
+
+
+class RegisterUserHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def post(self):
+        user_id = str(uuid.uuid4())
+        hoss_middleware.identify(user_id, { "created_at": datetime.datetime.now().isoformat()})
+        self.write(dict(userId=user_id))
+
+
+class TrackEventHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        user_id = self.request.query_arguments.get('userId')[0]
+        event = self.request.query_arguments.get('event')[0]
+        hoss_middleware.track(user_id, event, { "foo": "bar"})
 
 
 def get_user(handler):
@@ -50,7 +69,8 @@ def main():
     http_server = tornado.httpserver.HTTPServer(make_app())
 
     # initialize Hoss middleware before starting IOLoop
-    HossMiddleware.init(os.environ.get('HOSS_API_KEY'), {
+    global hoss_middleware
+    hoss_middleware = HossMiddleware.init(os.environ.get('HOSS_API_KEY'), {
         "USER_DATA_FN": get_user,
         "SHOULD_SKIP_FN": should_skip
     })
